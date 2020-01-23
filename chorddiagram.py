4891e4d4d9edca7e836064003ba0c2bd5268fd21
kevin <khynes57@outlook.com>
2020-01-22 21:23:14 -0500

    Basic version of ChordDiagram complete. Using a lot of kivy properties and semi-hardcoded graphics instructions in chorddiagram.kv.  May not be a problem?

--- chorddiagram.py
+++ chorddiagram.py
@@ -10,11 +10,42 @@ class ChordDiagram(Widget):
     draw_y = NumericProperty(0)
     step_x = NumericProperty(0)
     step_y = NumericProperty(0)
+    line_weight = NumericProperty(1)
     nut_height = NumericProperty(0)
     nut_opac = NumericProperty(1)
 
+    s6_open_mark_opac = NumericProperty(1)
+    s5_open_mark_opac = NumericProperty(1)
+    s4_open_mark_opac = NumericProperty(1)
+    s3_open_mark_opac = NumericProperty(1)
+    s2_open_mark_opac = NumericProperty(1)
+    s1_open_mark_opac = NumericProperty(1)
+
+    s6_x_mark_opac = NumericProperty(1)
+    s5_x_mark_opac = NumericProperty(1)
+    s4_x_mark_opac = NumericProperty(1)
+    s3_x_mark_opac = NumericProperty(1)
+    s2_x_mark_opac = NumericProperty(1)
+    s1_x_mark_opac = NumericProperty(1)
+
+    marker_radius = NumericProperty(0)
+    s6_marker_opac = NumericProperty(1)
+    s5_marker_opac = NumericProperty(1)
+    s4_marker_opac = NumericProperty(1)
+    s3_marker_opac = NumericProperty(1)
+    s2_marker_opac = NumericProperty(1)
+    s1_marker_opac = NumericProperty(1)
+
+    s6_red = NumericProperty(1)
+    s5_red = NumericProperty(1)
+    s4_red = NumericProperty(1)
+    s3_red = NumericProperty(1)
+    s2_red = NumericProperty(1)
+    s1_red = NumericProperty(1)
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
 
 
 class ChordDiagramContainer(FloatLayout):
